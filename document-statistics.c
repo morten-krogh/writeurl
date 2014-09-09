5c2fce53594615dd5854d0e2df01f632bf4c4dfe
Morten Krogh <morten.krogh@amberbio.com>
2014-09-09 20:10:35 +0200

    document-statistics

--- document-statistics.c
+++ document-statistics.c
@@ -9,34 +9,67 @@ char int_to_char (int n)
 	return n < 26 ? 'a' + n : '0' + (n - 26);
 }
 
-time_t mtime_for_path (char *path)
+struct file_times {
+	time_t mtime;
+	time_t birthtime;
+};
+
+struct file_times times_for_path (char *path)
 {
 	struct stat buf;
-	if (stat(path, &buf)) {
+	struct file_times times;
+
+	char path_noperation[200];
+	char path_ids[200];
+
+	sprintf(path_noperation, "%s/noperation", path);
+	sprintf(path_ids, "%s/ids", path);
+
+	if (stat(path_noperation, &buf)) {
 		//printf("Error in %s\n", path);
-		return 0;
+	} else {
+		times.mtime = buf.st_mtimespec.tv_sec;
 	}
 
-	return buf.st_mtimespec.tv_sec;
+	if (stat(path_ids, &buf)) {
+		//printf("Error in %s\n", path);
+	} else {
+		times.birthtime = buf.st_mtimespec.tv_sec;
+	}
+
+	return times;
 }
 
 struct statistics {
 	int total;
-	int day;
-	int week;
+	int mday;
+	int mweek;
+	int birthday;
+	int birthweek;
 };
 
 void update_path (char *path, struct statistics *statistic, time_t now)
 {
-	time_t mtime = mtime_for_path(path);
-	time_t timediff = now - mtime;
 
-	if (timediff < 24 * 3600) {
-		statistic->day++;
+	struct file_times times = times_for_path(path);
+
+	time_t mdiff = now - times.mtime;
+	time_t birthdiff = now - times.birthtime;
+
+	if (mdiff < 24 * 3600) {
+		statistic->mday++;
 	}
 
-	if (timediff < 7 * 24 * 3600) {
-		statistic->week++;
+	if (mdiff < 7 * 24 * 3600) {
+		statistic->mweek++;
+	}
+
+	if (birthdiff < 24 * 3600) {
+		statistic->birthday++;
+	}
+
+	if (birthdiff < 7 * 24 * 3600) {
+		statistic->birthweek++;
 	}
 
 	statistic->total++;
@@ -54,7 +87,7 @@ void update_dir (int d1, int d2, struct statistics *statistic, time_t now)
 	struct dirent *dp;
 	while ((dp = readdir(dirp)) != NULL) {	
 		if (dp->d_name[0] != '.') {
-			sprintf(path, "%s/%s/noperation", dir, dp->d_name);
+			sprintf(path, "%s/%s", dir, dp->d_name);
 			update_path (path, statistic, now);
 		}
 	}
@@ -63,7 +96,7 @@ void update_dir (int d1, int d2, struct statistics *statistic, time_t now)
 
 int main (int argv, char **argc) 
 {
-	struct statistics statistic = {0, 0, 0};
+	struct statistics statistic = {0, 0, 0, 0, 0};
 
 	time_t now = time(0);
 
@@ -75,7 +108,9 @@ int main (int argv, char **argc)
 	}
 
 	printf("The total number of documents is %i\n", statistic.total);
-	printf("Updated last 24 hours: %i\n", statistic.day);
-	printf("Updated last week: %i\n", statistic.week);
+	printf("Created last 24 hours: %i\n", statistic.birthday);
+	printf("Updated last 24 hours: %i\n", statistic.mday);
+	printf("Created last week: %i\n", statistic.birthweek);
+	printf("Updated last week: %i\n", statistic.mweek);
 	return 0;
 }
