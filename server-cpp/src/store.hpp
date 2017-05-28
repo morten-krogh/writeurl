878bd71e11f5c1522d7c5e810467dc4dc2fa4c33
Morten Krogh <morten.krogh@amberbio.com>
2017-05-28 18:13:41 +0200

    server-cpp

---
+++ server-cpp/src/store.hpp
@@ -0,0 +1,17 @@
+/*************************************************************
+ *
+ * Writeurl is an online collaborative editor.
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2017 Writeurl
+ *
+************************************************************/
+
+namespace writeurl {
+namespace store {
+
+
+
+
+} // namespace store
+} // namespace writeurl
