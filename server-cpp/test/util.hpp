adaf3b13b7e305e9da17e10e3f5049a8d0a67883
Morten Krogh <morten.krogh@amberbio.com>
2017-06-04 17:05:00 +0200

    build directory

---
+++ server-cpp/test/util.hpp
@@ -0,0 +1,26 @@
+/*************************************************************
+ *
+ * Writeurl is an online collaborative editor.
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2017 Writeurl
+ *
+************************************************************/
+
+#include <cstdlib>
+
+namespace writeurl {
+namespace test {
+
+std::string
+
+
+
+
+bool file_exists(const std::string& path);
+
+
+
+
+} // namespace test
+} // namespace writeurl
