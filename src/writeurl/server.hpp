2dfc89cbf26746462939a464f3657224b8ef5817
Morten Krogh <morten.krogh@amberbio.com>
2017-09-30 20:46:46 +0200

    spdlog subodule

---
+++ src/writeurl/server.hpp
@@ -0,0 +1,41 @@
+/*************************************************************
+ *
+ * Writeurl is an online collaborative editor.
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2017 Writeurl
+ *
+************************************************************/
+
+#ifndef WRITEURL_SERVER_H
+#define WRITEURL_SERVER_H
+
+#include <string>
+
+// #include <writeurl/store.hpp>
+
+
+namespace writeurl {
+
+class Server {
+public:
+
+    struct Config {
+
+
+
+
+    };
+
+    Server(const Config& config);
+
+
+
+private:
+
+
+};
+
+} // namespace writeurl
+
+#endif // WRITEURL_SERVER_H
